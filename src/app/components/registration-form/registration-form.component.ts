6e01b05566f1d434016cc5937e3c048a9f70e319
KitBlagov <115415701+KitBlagov@users.noreply.github.com>
2024-01-13 20:10:41 +0300

    Creating a Form

---
+++ src/app/components/registration-form/registration-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.scss'
+})
+export class RegistrationFormComponent {
+
+}
