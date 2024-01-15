ade414943dec1c2811d65dff236cdf629090c711
KitBlagov <115415701+KitBlagov@users.noreply.github.com>
2024-01-15 22:10:23 +0300

    create reistration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+
 
 @Component({
   selector: 'registration-form',
   templateUrl: './registration-form.component.html',
   styleUrl: './registration-form.component.scss'
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
+  registrationForm!: FormGroup;
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  get email() {
+    return this.registrationForm.get('emailAddress');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: [''],
+      emailAddress: [''],
+      phoneNumber: [''],
+      createPassword: [''],
+      confirmPassword: ['']
+    })
+  }
 
 }
