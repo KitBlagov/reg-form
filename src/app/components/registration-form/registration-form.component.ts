d1a8ea94eb31cb64d6777d521f195b4526b026dd
KitBlagov <115415701+KitBlagov@users.noreply.github.com>
2024-01-19 17:38:37 +0300

    Working with validators

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -7,7 +7,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
   templateUrl: './registration-form.component.html',
   styleUrl: './registration-form.component.scss'
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm: FormGroup;
 
   get fullName() {
@@ -18,30 +18,74 @@ export class RegistrationFormComponent {
     return this.registrationForm.get('emailAddress');
   }
 
+  get phone() {
+    return this.registrationForm.get('phoneNumber');
+  }
+
   get createPassword() {
     return this.registrationForm.get('createPassword');
   }
 
-  passwordValidator(control: FormControl): {[s: string]: boolean} | null {
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword')
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = new FormGroup({});
+  }
+
+  ngOnInit() {
+    this._createForm()
+  }
+
+  private _createForm() {
+
+
+
+    this.registrationForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(5), Validators.pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/), this.checkFullNameValidator]],
+      emailAddress: ['', [Validators.required, Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/), Validators.minLength(5)]],
+      phoneNumber: ['',[Validators.required, Validators.minLength(10), Validators.maxLength(15), Validators.pattern(/^\+7[0-9]{10}$/)]],
+      createPassword: ['', [Validators.required, Validators.minLength(8), this.passwordValidator]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
+
+  checkFullNameValidator(control: FormControl) {
+    if(!control.value.includes(' ')) {
+      return {'fullName': true};
+    }
+    return null;
+  }
+  passwordValidator(control: FormControl): { [s: string]: boolean } | null {
     const regex = new RegExp('[@!#$%^&*()<>?/\|}{~:]');
 
-    if(!regex.test(control.value)) {
-      return { 'password': true };
+    if (!regex.test(control.value)) {
+      return {'password': true};
     }
     return null;
   }
 
-  constructor(private fb: FormBuilder) {
-    this.registrationForm = this.fb.group({
-      fullName: ['', [Validators.required]],
-      emailAddress: [''],
-      phoneNumber: [''],
-      createPassword: ['', [Validators.required, Validators.minLength(8), this.passwordValidator]],
-      confirmPassword: ['', [Validators.required]]
-    });
+  passwordMatchValidator(formGroup: FormGroup) {
+
+    const password = formGroup.get('createPassword');
+    const confirmPassword = formGroup.get('confirmPassword');
+
+    if(!password || !confirmPassword) {
+      return null;
+    }
+
+    if(password.value !== confirmPassword.value) {
+      return {
+        passwordMismatch: true
+      };
+    }
+
+    return null;
+
   }
-
-
 
 
 }
